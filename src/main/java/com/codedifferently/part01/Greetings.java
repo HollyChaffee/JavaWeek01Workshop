ce1ad069c6397ae3d5518550f346e94230fcce0f
HollyChaffee <saltdc95@gmail.com>
2020-10-24 20:26:07 +0000

    2nd commit

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,17 @@
 package com.codedifferently.part01;
 
+
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name");
+        String name = scanner.next();
+        if (name.equals("Hakim") || name.equals("Rasheeda")) {
+            System.out.println("Hello");
+        }
 
     }
 }
